7b52e3e773a9b0072de30fc8da1ff81d689448a0
Calvin Flores <calvin.flores@ocsbstudent.ca>
2017-06-07 09:54:57 -0400

    6/7/17 Level 1 Done w/ Level Transition

---
+++ ZombieToys/Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTransition : MonoBehaviour {
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        Application.LoadLevel("22");
+            }
+        }
+    

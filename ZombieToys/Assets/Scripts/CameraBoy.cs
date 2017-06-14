4102d4b93e84c03ac3fd8b1840c90debcd086067
Calvin Flores <calvin.flores@ocsbstudent.ca>
2017-06-14 09:49:19 -0400

    6/14/17 Level 2 Camera Change

---
+++ ZombieToys/Assets/Scripts/CameraBoy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBoy : MonoBehaviour {
+
+    public GameObject Boy;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - Boy.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Boy.transform.position + offset;
+    }
+}

294c3c43cfcd66b808cadad0568496795422780c
The Applerun <calvin.flores@ocsbstudent.ca>
2017-06-16 21:14:01 -0400

    6/16/17 Level 2 & Credits done

---
+++ ZombieToys/Assets/Scripts/LevelTransistion3.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTransistion3 : MonoBehaviour {
+
+
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        Application.LoadLevel("Credits");
+    }
+}

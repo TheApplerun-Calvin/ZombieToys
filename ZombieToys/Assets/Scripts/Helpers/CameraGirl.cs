5c086d090c80ad1fa6691d4fd78bf6d9dc920f48
Calvin Flores <calvin.flores@ocsbstudent.ca>
2017-06-21 14:22:54 -0400

    6/21/17 Credits Text Fix

---
+++ ZombieToys/Assets/Scripts/Helpers/CameraGirl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraGirl : MonoBehaviour {
+
+    public GameObject Girl;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - Girl.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Girl.transform.position + offset;
+    }
+}
